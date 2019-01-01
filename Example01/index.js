7bbbba9a882a0b9462488ee9eb98fa03e8db57f4
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2019-01-01 22:04:28 +1300

    Create index.js

---
+++ Example01/index.js
@@ -0,0 +1,21 @@
+1. html tag is component in react-native
+
+HTML
+-----
+<p> </p>
+<img> </img>
+
+React-native
+-------------
+<Image>
+<View></View>
+
+2. html tag attribute is props in react-native
+
+HTML
+-----
+<img src="#" />
+
+React-native
+-------------
+<Image source={pic} />
