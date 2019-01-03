e41295f5db14c5103d89ac4fa12db4f9c6e34d77
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2019-01-04 05:27:18 +1300

    Update index.js

--- Example01/FlatList/index.js
+++ Example01/FlatList/index.js
@@ -1,3 +1,24 @@
+/* FlatList component works well for long lists of data. It requires two props 'data' and 'renderItem'. 
+   
+   'data' is the source of information for the list.
+
+		data={[
+        {key: 'Devin'},
+        {key: 'Jackson'},
+        {key: 'James'},
+        {key: 'Joel'},
+        {key: 'John'},
+        {key: 'Jillian'},
+        {key: 'Jimmy'},
+        {key: 'Julie'},
+      ]}
+
+	'renderItem' takes one item from 'data' and format it.
+
+		renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+
+*/ 
+
 import React, { Component } from 'react';
 import { AppRegistry, Text, View, StatusBar, FlatList } from 'react-native';
 
